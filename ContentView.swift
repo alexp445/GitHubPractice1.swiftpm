f641137d5eca4835156ffd15ce8382c3df07151e
Niko D <moki.gaming12@gmail.com>
2026-04-13 10:53:57 -0500

    Deleted the names and added title to the top of the app.

--- ContentView.swift
+++ ContentView.swift
@@ -2,7 +2,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Alex")
-        Text("NIKO")
+        VStack {
+            Text("This Project s a simple project!")
+                .font(Font.largeTitle)
+            Spacer()
+        }
+        
+        
     }
 }
